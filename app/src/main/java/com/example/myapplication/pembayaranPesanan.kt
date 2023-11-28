9f8b2bf784a75039e046d529e46489fcc144924f
aqil213 <qilaz1881@gmail.com>
2023-11-28 19:10:45 +0700

    File Baru

---
+++ app/src/main/java/com/example/myapplication/pembayaranPesanan.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class pembayaranPesanan : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pembayaran_pesanan)
+    }
+}
